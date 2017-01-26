5a4899b131bba136a575de3f235f89593d71bfda
tonietto <tonietto@gmail.com>
2017-01-26 16:08:24 -0200

    Correct typo

--- purity.zsh
+++ purity.zsh
@@ -101,15 +101,15 @@ prompt_purity_setup() {
 	ZSH_THEME_GIT_PROMPT_DIRTY=""
 	ZSH_THEME_GIT_PROMPT_CLEAN=""
 
-	ZSH_THEME_GIT_PROMPT_ADDED="%F{green}✓%f"
-	ZSH_THEME_GIT_PROMPT_MODIFIED="%F{blue}✶%f"
-	ZSH_THEME_GIT_PROMPT_DELETED="%F{red}✗%f"
-	ZSH_THEME_GIT_PROMPT_RENAMED="%F{magenta}➜%f"
-	ZSH_THEME_GIT_PROMPT_UNMERGED="%F{yellow}═%f"
-	ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{cyan}✩%f"
+	ZSH_THEME_GIT_PROMPT_ADDED="%F{green}✓%"
+	ZSH_THEME_GIT_PROMPT_MODIFIED="%F{blue}✶%"
+	ZSH_THEME_GIT_PROMPT_DELETED="%F{red}✗%"
+	ZSH_THEME_GIT_PROMPT_RENAMED="%F{magenta}➜%"
+	ZSH_THEME_GIT_PROMPT_UNMERGED="%F{yellow}═%"
+	ZSH_THEME_GIT_PROMPT_UNTRACKED="%F{cyan}✩%"
 
 	# prompt turns red if the previous command didn't exit with 0
-	PROMPT='%(?.%F{green}.%F{red})❯% f%F{blue}%c$(git_prompt_info)$(git_prompt_status) '
+	PROMPT='%(?.%F{green}.%F{red})❯%f %F{blue}%c$(git_prompt_info)$(git_prompt_status) '
 	RPROMPT='%F{red}%(?..⏎)%f'
 }
 
